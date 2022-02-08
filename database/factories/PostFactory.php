a247186ac684a2c9c6cf795fde384cdc133f1cd8
tatuya4451 <tatuya44511336@gmail.com>
2022-02-08 18:33:37 +0900

    add: ダミーデータの作成

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -21,8 +21,15 @@ class PostFactory extends Factory
      */
     public function definition()
     {
+        $random_date = $this->faker->dateTimeBetween('-1year', '-1day');
+
         return [
-            //
+            'title' => $this->faker->realText(rand(20,50)),
+            'body' => $this->faker->realText(rand(100,200)),
+            'is_public' => $this->faker->boolean(90),
+            'published_at' => $random_date,
+            'created_at' => $random_date,
+            'updated_at' => $random_date
         ];
     }
 }
