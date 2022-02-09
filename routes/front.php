c8d18d01ebe6a77c6e502ec767a1985fd0382bd0
tatuya4451 <tatuya44511336@gmail.com>
2022-02-09 15:28:49 +0900

    fix

--- routes/front.php
+++ routes/front.php
@@ -1,6 +1,5 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'front';
-});
+Route::get('/', 'PostController@index')->name('home');
+Route::resource('posts', 'PostController')->only(['index','show']);
