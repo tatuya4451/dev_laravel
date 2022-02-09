f9412bcd73780ac938ccf53221e332788753314c
tatuya4451 <tatuya44511336@gmail.com>
2022-02-09 17:25:25 +0900

    ログイン機能追加

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -176,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\FortifyServiceProvider::class,
     ],
 
     /*
@@ -227,9 +227,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-
     ],
 
 ];
--- resources/views/front/posts/index.blade.php
+++ resources/views/front/posts/index.blade.php
@@ -15,7 +15,7 @@ $title = '投稿一覧';
         <table class="table">
             @foreach($posts as $post)
                 <tr>
-                    <td>{{ $post->published_at->format('Y年m月d日') }}</td>
+                    <td>{{ $post->published_format }}</td>
                     <td>{!! link_to_route('front.posts.show', $post->title, $post) !!}</td>
                 </tr>
             @endforeach
