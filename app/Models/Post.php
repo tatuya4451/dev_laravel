f63e7717eef832e969135eb5a64d0d95471c6821
tatuya4451 <tatuya44511336@gmail.com>
2022-02-09 15:10:42 +0900

    一旦コミット

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Post extends Model
 {
@@ -17,4 +18,22 @@ class Post extends Model
         'is_public' => 'bool',
         'published_at' => 'datetime'
     ];
+
+    public function scopePublic(Builder $query)
+    {
+        return $query->where('is_public', true);
+    }
+
+    public function scopePublicList(Builder $query)
+    {
+        return $query
+            ->public()
+            ->latest('published_at')
+            ->paginate(10);
+    }
+
+    public function scopePublicFindById(Builder $query, int $id)
+    {
+        return $query->public()->findOrFail($id);
+    }
 }
